1804ff6f81bf5e8948ee378cc1a769f8526aa4a7
mayur <mak.99@outlook.com>
2021-07-23 16:17:03 +0530

    intial push

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Header() {
+    return (
+        <header>
+            {/* Top nav */}
+        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+            <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                <Image
+                src="https://links.papareact.com/f90"
+                width={150}
+                height={40}
+                objectFit="contain"
+                className="cursor-pointer"
+                />
+            </div>
+            {/* search bar */}
+            <div>
+                <input type="text"/>
+            </div>
+        </div>
+        {/* nav bottom */}
+        <div></div>
+        </header>
+    )
+}
+
+export default Header
